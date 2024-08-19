2ecd7b7469fae3dae96c11e4e4b1576179b6ad94
Ripon Kumar Saha <48709421+Riponcs@users.noreply.github.com>
2024-08-19 12:03:24 -0500

    Updated Journal and Skills

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ jQuery('.skillbar').each(function () {
         width: jQuery(this).attr('data-percent')
     }, 2000);
 });
+
+// Some script added by Ripon
+function scrollToClass(className) {
+    const element = document.querySelector(`.${className}`);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
